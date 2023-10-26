c7542f3f46cb1397f3c51cfb05e3797f65df6227
jbryan83 <james.bryan.home@gmail.com>
2023-10-26 06:52:28 -0500

    Added backdrop blur to elements

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -14,7 +14,7 @@ interface SiteHeaderProps {
 export async function SiteHeader({ items }: SiteHeaderProps) {
   const session = await getUserAuth();
   return (
-    <header className='sticky top-0 z-40 w-full border-b bg-background'>
+    <header className='sticky top-0 z-40 w-full border-b bg-background/80 backdrop-blur'>
       <div className='container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0'>
         <MainNav items={items} />
         <div className='flex flex-1 items-center justify-end space-x-4'>
